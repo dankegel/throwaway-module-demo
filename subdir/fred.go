f6965de279460f4c7db16ef5055b1484ae5a8889
Dan Kegel <dank@kegel.com>
2021-03-09 18:10:57 -0800

    Dummy go package in subdirectory

---
+++ subdir/fred.go
@@ -0,0 +1,7 @@
+package fred
+
+import "fmt"
+
+func Hello(name string) {
+	fmt.Println("hello %v from subdir's fred", name)
+}
---
+++ subdir/go.mod
@@ -0,0 +1,3 @@
+module github.com/dkegel/throwaway-module-demo/subdir
+
+go 1.16
